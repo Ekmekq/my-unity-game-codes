a865e2c2d2fb272c23a30ee7ad6f1246e067dabd
Ata Cem KILIÇ <67519722+Ekmekq@users.noreply.github.com>
2020-07-20 00:07:05 +0300

    my games codes
    lmao xdxdxdxdxdxd

---
+++ DragToShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragToShoot : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    Vector2 startpos;
+    Vector2 endpos;
+    [SerializeField] float power = 2f; // güç
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            endpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            LaunchBall();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        startpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void LaunchBall()
+    {
+        Vector2 direction = (startpos - endpos).normalized; // yön ayarla
+        rbody.AddForce(direction * power, ForceMode2D.Impulse);
+    }
+}
